624a19c5377dfd2a041630f247540024ee6b7291
Rafael Albuquerque <rafael.silva.albuquerque@hotmail.com>
2024-12-17 21:34:18 -0300

    initial commit

---
+++ __main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def main():
+    url = "https://discord.com/api/download/stable?platform=linux&format=deb"
+
+    subprocess.run(['wget', url, '-O', '/tmp/discord.deb'])
+    subprocess.run(['sudo', 'dpkg', '-i', f'/tmp/discord.deb'])
+
+    print("Updated Discord!")
+
+
+if __name__ == "__main__":
+    main()
